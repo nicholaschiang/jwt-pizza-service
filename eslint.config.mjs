652ae816881d5e94ca753ffeca7facc2d90711ce
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2024-09-30 15:35:10 -0600

    test: setup basic jest testing suite

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  { files: ["**/*.test.js"], env: { jest: true } },
   pluginJs.configs.recommended,
 ];
